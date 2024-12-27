b2dc12766fdb2aeccfc17e6d95e758274331007d
1kkiren <1kkiren@mail.ru>
2024-12-28 02:27:30 +0300

    [Update] theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ language = 'Python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 html_static_path = ['_static']
 
