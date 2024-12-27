fbc14f1ed92b26c309f598877edbd2a69a56afac
1kkiren <1kkiren@mail.ru>
2024-12-28 01:14:18 +0300

    [Add] conf

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+# Sphinx configuration file
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../TokenizerChanger'))
+
+project = 'TokenizerChanger'
+author = '1kkiren'
+version = '0.4.0'
+release = '0.4.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
